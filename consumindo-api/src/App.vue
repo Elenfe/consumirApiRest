3818f5a71c19f3b03133e0a7b59aba409d5cb443
Élenfe Gomes da Silva <elenfe.silva@medbarra.com.br>
2019-01-03 20:11:23 -0200

    novo ajuste

--- consumindo-api/src/App.vue
+++ consumindo-api/src/App.vue
@@ -3,52 +3,80 @@
 
     <nav>
       <div class="nav-wrapper blue darken-1">
-        <a href="#" class="brand-logo center">filtro aprovações</a>
+        <div>
+          <div class="brand-logo center">FILTRO DE APROVAÇÔES</div>          
+        </div>
+        <button class="waves-effect btn-small darken-1 right" @click="show=true">Cadastrar nova Aprovação</button>
       </div>
     </nav>
 
     <div class="container">
+      <div class="row">
+        <div class="col s3">
+          <label>Selecione o Ano</label>
+          <select class="browser-default md-raised">
+            <option value="1">2018</option>
+            <option value="2">2017</option>
+            <option value="3">2016</option>
+            <option value="4">2015</option>
+          </select>
+        </div>
 
-      <div class="row">      
-      <div class="col s6 offset-s3"></div>
-      <div class="col s6 offset-s3"></div>
-      <div class="col s6 offset-s3"></div>
-      <div class="col s6 offset-s3"></div>
-    </div>
+        <div class="col s3">
+          <label>Selecione o Estado</label>
+          <select class="browser-default md-raised">
+            <option value="1">2018</option>
+            <option value="2">2017</option>
+            <option value="3">2016</option>
+            <option value="4">2015</option>
+          </select>
+        </div>
 
-      <form>
+        <div class="col s3">
+          <label>Selecione o Concurso</label>
+          <select class="browser-default md-raised"
+                :value="null"
+                :options="aprovacoes"
+                required
+                v-model="aprovacoes.year">                
+          </select>
+        </div>
 
-          <label>Nome</label>
-          <input type="text" placeholder="Nome">
-          <label>Quantidade</label>
-          <input type="number" placeholder="QTD">
-          <label>Valor</label>
-          <input type="text" placeholder="Valor">
+        <div class="col s3">
+          <label>Sigla</label>
+          <select class="browser-default md-raised">
+            <option value="1">2018</option>
+            <option value="2">2017</option>
+            <option value="3">2016</option>
+            <option value="4">2015</option>
+          </select>      
+        </div>
+      </div>
 
-          <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
-
-      </form>
+        <div class="row">
+          <div class="col s12">UFRJ - 2018 <br> Universidade Federal do Rio de Janeiro <br> Residência Médica</div>          
+        </div>
+        <div class="row">          
+          <div class="col s12"><h3>RESULTADO FINAL</h3><br>APROVADO EM NEGRITO</div>          
+        </div>
+        <div class="row">          
+          <div class="col s12">HOSPITAL UNIVERSITÁRIO CLEMENTE FRAGA FILHO</div>          
+        </div>
+        <div class="row">          
+          <div class="col s12">ANESTISIOLOGIA</div>          
+        </div>
+        <div class="row">          
+          <div class="col s12">TODOS OS APROVADOS FORAM DO MEDGRUPO!!</div>
+        </div>    
 
       <table>
 
-        <thead>
-
-          <tr>
-            <th>NOME</th>
-            <th>QTD</th>
-            <th>VALOR</th>
-            <th>OPÇÕES</th>
-          </tr>
-
-        </thead>
-
         <tbody>
+            <tr>
 
-          <tr>
-
-            <td>Arduino</td>
-            <td>100</td>
-            <td>50.00</td>
+            <td>1</td>
+            <td>Élenfe Gomes da silva</td>
+            <td>MEDGRUPO</td>
             <td>
               <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
               <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
@@ -60,31 +88,36 @@
       
       </table>
 
-      <label>Browser Select</label>
-  <select class="browser-default">
-    <option value="" disabled selected>Choose your option</option>
-    <option value="1">Option 1</option>
-    <option value="2">Option 2</option>
-    <option value="3">Option 3</option>
-  </select>
-
     </div>
 
-    <label>Browser Select</label>
-  <select class="browser-default">
-    <option value="" disabled selected>Choose your option</option>
-    <option value="1">Option 1</option>
-    <option value="2">Option 2</option>
-    <option value="3">Option 3</option>
-  </select>
-
   </div>
+
+  
 </template>
 
 <script>
 
-</script>
+  import Aprovacoes from './services/aprovacoes'
 
+  export default {
+
+    data(){
+      return {
+        aprovacoes: [],
+        show: false
+      }
+      
+    },
+
+    mounted(){
+      Aprovacoes.listar().then(resposta => {
+        console.log(resposta.data)
+        this.Aprovacoes = resposta.data
+      })
+    }
+  }
+</script>
+ 
 <style>
 
 </style>
---
+++ consumindo-api/src/services/aprovacoes.js
@@ -0,0 +1,9 @@
+import { http } from './config'
+
+export default {
+
+  listar:() => {
+    return http.get('5c1bcea01981cf370444a19f')
+  }
+
+}
