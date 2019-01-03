7f6bfe4191b4d11e0eb9fb211db474d138ae9240
elenfe <elenfe7@gmail.com>
2019-01-03 01:35:04 -0200

    primeiro projeto

---
+++ consumindo-api/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+
+    <nav>
+      <div class="nav-wrapper blue darken-1">
+        <a href="#" class="brand-logo center">filtro aprovações</a>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="row">      
+      <div class="col s6 offset-s3"></div>
+      <div class="col s6 offset-s3"></div>
+      <div class="col s6 offset-s3"></div>
+      <div class="col s6 offset-s3"></div>
+    </div>
+
+      <form>
+
+          <label>Nome</label>
+          <input type="text" placeholder="Nome">
+          <label>Quantidade</label>
+          <input type="number" placeholder="QTD">
+          <label>Valor</label>
+          <input type="text" placeholder="Valor">
+
+          <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+
+      </form>
+
+      <table>
+
+        <thead>
+
+          <tr>
+            <th>NOME</th>
+            <th>QTD</th>
+            <th>VALOR</th>
+            <th>OPÇÕES</th>
+          </tr>
+
+        </thead>
+
+        <tbody>
+
+          <tr>
+
+            <td>Arduino</td>
+            <td>100</td>
+            <td>50.00</td>
+            <td>
+              <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
+              <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
+            </td>
+
+          </tr>
+
+        </tbody>
+      
+      </table>
+
+      <label>Browser Select</label>
+  <select class="browser-default">
+    <option value="" disabled selected>Choose your option</option>
+    <option value="1">Option 1</option>
+    <option value="2">Option 2</option>
+    <option value="3">Option 3</option>
+  </select>
+
+    </div>
+
+    <label>Browser Select</label>
+  <select class="browser-default">
+    <option value="" disabled selected>Choose your option</option>
+    <option value="1">Option 1</option>
+    <option value="2">Option 2</option>
+    <option value="3">Option 3</option>
+  </select>
+
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
